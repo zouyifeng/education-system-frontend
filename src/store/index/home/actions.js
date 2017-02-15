dc933937a65813d4c81c7f2a0b4175c0c1110000
zouyifeng <zouyifeng@kuaiyd.com>
2017-02-15 19:47:15 +0800

    t

--- src/store/index/home/actions.js
+++ src/store/index/home/actions.js
@@ -4,19 +4,19 @@ import * as types from './mutation-types'
 
 export default {
     fetchAccount({ commit }, account) {
-        const url = '/admin/login.action';
+        const url = '/api/admin/login.action';
         util.post({ url }, account).then((resp) => {
             commit(types.FETCH_ACCOUNT, resp.body.data);
         });
     },
     getNews({ commit }) {
-        const url = '/news_list.action';
+        const url = '/api/news_list.action';
         util.get({ url }).then((resp) => {
             commit(types.FETCH_NEWS_LIST, resp.body.data);
         })
     },
     getNewsDetail({ commit }, id) {
-        const url = '/news.action';
+        const url = '/api/news.action';
         util.post({ url }, id).then((resp) => {
             commit(types.FETCH_NEWS_DETAIL, resp.body.data)
         })
--- src/store/util.js
+++ src/store/util.js
@@ -1,11 +1,11 @@
 import vue from 'vue'
 
-const _urlPrefix = 'http://localhost:8081/wechat-education-system';
+// const _urlPrefix = 'http://localhost:8081/wechat-education-system';
+const _urlPrefix = '';
 
 export const get = ({ url }) => {
     return vue.http.get( _urlPrefix + url );
 }
-
 
 export const post = ({ url }, data) => {
     return vue.http.post( _urlPrefix + url, { data: data } );
