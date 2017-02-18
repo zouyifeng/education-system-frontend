a093f87172fc746423189e30da24c40e4209d57f
zouyifeng <zouyifeng@aliyun.com>
2017-02-18 08:38:42 +0800

    gh

--- src/components/common/MessageBox.vue
+++ src/components/common/MessageBox.vue
@@ -1,9 +1,44 @@
 <template>
-    
+    <el-button type="text" @click="msg">{{}}</el-button>
 </template>
 <script>
     import { MessageBox } from 'element-ui'
 
-    
+    export default {
+        props: {
+            action: {
+                type: [String],
+                required: true
+            },
+            callback: {
+                default() {
+                    return function callback() {
+                        // todo
+                    }
+                }
+            },
+             methods: {
+                msg() {
+                    MessageBox.confirm('此操作将永久删除该项, 是否继续?', '提示', {
+                        confirmButtonText: '确定',
+                        cancelButtonText: '取消',
+                        type: 'warning',
+                        callback: 'callback'
+                    }).then(() => {
+                        MessageBox({
+                            type: 'success',
+                            message: '删除成功!'
+                        });
+                    }).catch(() => {
+                        MessageBox({
+                            type: 'info',
+                            message: '已取消删除'
+                        });          
+                    });
+                }
+                }
+        }
+    }
+
 
 </script> 
